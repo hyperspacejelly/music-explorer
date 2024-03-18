95ae331300bfdaa05b56c5cb3dc418b367a4cec2
Lucien Jély <lucien.jely@gmail.com>
2024-03-18 18:02:02 +0100

    Update API_ENDPOINT.ts

--- src/API_ENDPOINT.ts
+++ src/API_ENDPOINT.ts
@@ -1,11 +1,11 @@
 // the real one is private. C'mon.
 
-const baseURL = "https://www.domain.extension/api";
+const baseURL = "http://127.0.0.1/musex-api";
 
-export const get_albums = baseURL+"/subfolder1/get_albums.php";
-export const like_album = baseURL+"/subfolder1/like_album.php";
-export const discogs_fetch = baseURL+"/subfolder1/get_discogs_info.php";
-export const login_check = baseURL+"/subfolder2/login.php";
-export const user_info = baseURL+"/subfolder2/get_user_info.php";
-export const user_info_update = baseURL+"/subfolder2/update_user_info.php";
-export const create_user = baseURL+"/subfolder2/create_user.php";
+export const get_albums = baseURL+"/data/get_albums.php";
+export const like_album = baseURL+"/data/like_album.php";
+export const discogs_fetch = baseURL+"/data/get_discogs_info.php";
+export const login_check = baseURL+"/user/login.php";
+export const user_info = baseURL+"/user/get_user_info.php";
+export const user_info_update = baseURL+"/user/update_user_info.php";
+export const create_user = baseURL+"/user/create_user.php";
